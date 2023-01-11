9a568d30b5ca7fd93b158f97a07dd234ea531776
faisalkhan <faisalkhanthayub@gmail.com>
2023-01-11 09:12:16 +0100

    spring-boot-v3 google cloud run hello world

---
+++ gcloud-run-spring-boot-v3-native/src/main/java/be/techroots/gcloudrun/GcloudRunApplication.java
@@ -0,0 +1,25 @@
+package be.techroots.gcloudrun;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+public class GcloudRunApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GcloudRunApplication.class, args);
+    }
+
+    @RestController
+    public class HelloResource {
+
+        @GetMapping("/hello")
+        public String hello() {
+            return "hello";
+        }
+
+    }
+}
---
+++ gcloud-run-spring-boot-v3-native/src/test/java/be/techroots/gcloudrun/GcloudRunApplicationTests.java
@@ -0,0 +1,13 @@
+package be.techroots.gcloudrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GcloudRunApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
